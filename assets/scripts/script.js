94a6077613e1525a422d944da96d02124d6628de
anasvemmully <anasvemmully@gmail.com>
2025-12-24 15:22:44 +0530

    feat: add morphing transition to word animation
    - Replace erase-and-restart with smooth morphing
    - Add character-by-character transformation
    - Create dynamic shuffling between words
    - Improve visual continuity of animation

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,7 +4,6 @@
     ele_passions = document.getElementById("passion");
 
     var shuffle = (ele) => {
-        // "use strict";
         var m = this
         m.init = () => {
             m.code = "&#*+%?£@§$"
@@ -17,29 +16,29 @@
                 "innovations",
                 "solutions",
             ]
-            setTimeout(m.animateIn, 100);        
+            setTimeout(m.animateIn, 100);
         }
 
         m.generateRandomString = (length) => {
             var random_text = '';
             while(random_text.length < length){
               random_text += m.code.charAt(Math.floor(Math.random()*m.code.length));
-            } 
+            }
             return random_text;
-          };
+        };
 
         m.animateIn = () => {
             if(m.current_length < m.messages[m.message].length){
                 m.current_length = m.current_length + 2;
                 if(m.current_length > m.messages[m.message].length) {
-                m.current_length = m.messages[m.message].length;
+                    m.current_length = m.messages[m.message].length;
                 }
-                
+
                 var message = m.generateRandomString(m.current_length);
                 ele.innerHTML = message
-                
+
                 setTimeout(m.animateIn, 20);
-            } else { 
+            } else {
                 setTimeout(m.animateFadeBuffer, 20);
             }
         };
@@ -51,10 +50,10 @@
                 m.fadeBuffer.push({c: (Math.floor(Math.random()*12))+1, l: m.messages[m.message].charAt(i)});
               }
             }
-            
+
             var do_cycles = false;
-            var message = ''; 
-            
+            var message = '';
+
             for(var i = 0; i < m.fadeBuffer.length; i++){
               var fader = m.fadeBuffer[i];
               if(fader.c > 0){
@@ -65,35 +64,77 @@
                 message += fader.l;
               }
             }
-            
+
             ele.innerHTML = message
-            
+
             if(do_cycles === true){
               setTimeout(m.animateFadeBuffer, 50);
             } else {
               setTimeout(m.cycleText, 2000);
             }
-          };
+        };
 
-          m.cycleText = () => {
-            m.message = m.message + 1;
-            if(m.message >= m.messages.length){
-              m.message = 0;
+        m.cycleText = () => {
+            var nextMessage = m.message + 1;
+            if(nextMessage >= m.messages.length){
+              nextMessage = 0;
             }
-            
-            m.current_length = 0;
-            m.fadeBuffer = false;
-            ele.innerHTML = '';
-            
-            setTimeout(m.animateIn, 200);
-          };
 
-          m.init();
+            m.animateTransition(nextMessage);
+        };
 
+        m.animateTransition = (nextMessageIndex) => {
+            var currentWord = m.messages[m.message];
+            var nextWord = m.messages[nextMessageIndex];
+            var maxLength = Math.max(currentWord.length, nextWord.length);
+
+            if(!m.transitionBuffer) {
+                m.transitionBuffer = [];
+                m.transitionStep = 0;
+
+                // Create transition buffer for each character position
+                for(var i = 0; i < maxLength; i++){
+                    m.transitionBuffer.push({
+                        current: i < currentWord.length ? currentWord.charAt(i) : '',
+                        target: i < nextWord.length ? nextWord.charAt(i) : '',
+                        cycles: (Math.floor(Math.random()*8))+5
+                    });
+                }
+            }
+
+            var message = '';
+            var stillAnimating = false;
+
+            for(var i = 0; i < m.transitionBuffer.length; i++){
+                var char = m.transitionBuffer[i];
+
+                if(char.cycles > 0){
+                    stillAnimating = true;
+                    char.cycles--;
+                    message += m.code.charAt(Math.floor(Math.random()*m.code.length));
+                } else if(char.target !== ''){
+                    message += char.target;
+                }
+            }
+
+            ele.innerHTML = message;
+
+            if(stillAnimating){
+                setTimeout(() => m.animateTransition(nextMessageIndex), 40);
+            } else {
+                // Transition complete, update message index
+                m.message = nextMessageIndex;
+                m.current_length = 0;
+                m.fadeBuffer = false;
+                m.transitionBuffer = false;
+
+                setTimeout(m.animateIn, 200);
+            }
+        };
+
+        m.init();
     }
 
-
     shuffle(ele_passions)
-        
 
 })();
